c474e6b44c08865c6547cd5911c63c8e335a3b1d
munjal patel <munjalpatel@munjal-patels-MacBook-Pro.local>
2016-09-15 12:04:29 +0530

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase): #1
@@ -17,17 +18,32 @@ class NewVisitorTest(unittest.TestCase): #1
 
 		# HE noices the page title an header mention to-do list
 		self.assertIn('To-Do', self.browser.title) #5
-		self.fail('Finish the test!') #6
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text) #6
 
 		# He is invited to enter a to-do item staraight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a to-do item'
+			)
 
 		# He type "learn TDD with python" (he's and avid learning)
-
+		inputbox.send_keys('learn TDD with python')
+		
 		# When he hits enter, the page updates, and now the page lists
 		# 1. "learn TDD with python" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1. learn TDD with python' for row in rows)	
+			)
 
 		# There is still a text box inviting him to add another item.
 		# He enters "Search for relevant tutorials"
+		self.fail('Finish the test!')
 
 		# The page updates again, and now shows both items on his list
 
